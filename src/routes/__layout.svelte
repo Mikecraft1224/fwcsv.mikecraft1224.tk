4cad2d931bbd133f2febea71aa469f334fe1719a
Mikecraft1224 <Mikecraft1224@gmail.com>
2022-06-28 19:29:28 +0200

    first commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,31 @@
+<script>
+    import Navbar from "$lib/navbar.svelte";
+    import Footer from "$lib/footer.svelte";
+
+    import '../app.postcss';
+</script>
+
+<svelte:head>
+    <link rel='icon' type='image/vnd.microsoft.icon' href='/Icon.ico'>
+</svelte:head>
+
+<Navbar />
+
+<slot />
+
+<Footer />
+
+<style>
+	:global(body) {
+		--main-background: #111;
+
+		background-image: url('/code_wallpaper.svg');
+		background-repeat: no-repeat;
+		background-attachment: fixed;
+		background-position: center;
+		background-size: auto;
+
+		background-color: var(--main-background);
+    color: #ffffff;
+	}
+</style>
